c216ec7935622dee4d8681d64ee700f91be4b18a
anil kumar <akthinq@gmailcom>
2025-05-17 21:34:25 +0530

    updated

--- functions.sh
+++ functions.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-install_packege(){
-    dnf install nginx -y
-}
+var1=$1
+var2=$2
 
-install_packege()
+if [var1 -eq var2]
+then
+    echo "var1 and var2 both are have same value"
+fi
+
+# install_packege(){
+#     dnf install nginx -y
+# }
+
+# install_packege()
