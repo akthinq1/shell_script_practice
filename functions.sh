7d40409a779400e7dea066664887c1487b56a8ca
anil kumar <akthinq@gmailcom>
2025-05-19 15:05:01 +0530

    practice

--- functions.sh
+++ functions.sh
@@ -3,13 +3,8 @@
 var1=$1
 var2=$2
 
-if [ $var1 -eq $var2 ]
-then
-    echo "var1 and var2 both are have same value"
-fi
+Hello () {
+    echo "Hello world!"
+}
 
-# install_packege(){
-#     dnf install nginx -y
-# }
-
-# install_packege()
+Hello
