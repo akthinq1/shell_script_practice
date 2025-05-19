a194e43fc9ac57388c12dc49a91c2ba3500a2987
anil kumar <akthinq@gmailcom>
2025-05-19 15:35:48 +0530

    practi

--- functions.sh
+++ functions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 var=$1
+TimeStamps=$(date)
 
 Hello () {
     echo "Hello world! $1 $var"
@@ -11,4 +12,10 @@ Hello Anil
 
 ret=!?
 
+print_date () {
+    echo "current time and date : $TimeStamps"
+}
+
 echo "print return $ret"
+
+print_date
