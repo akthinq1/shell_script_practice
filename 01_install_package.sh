fe9bf2e8bf9c18c820e9a9a14978f2cd7a77763d
anil kumar <akthinq@gmailcom>
2025-05-20 10:31:54 +0530

    install package

---
+++ 01_install_package.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+
+# check super/root user or not
+#hint -> if root user "id -u == 0"
+
+check_user=$(id -u)
+
+if [$check_user -ne 0]
+then
+    echo "error:: pleae run the cmd with root access"
+    exit 1 #status for failue # exit status other than 0 upto 127
+else
+    echo "script running with root access"
+fi
+
+#create a validate function for success or failure the cmd
+
+validate(){
+    if [ $1 -eq 0]
+    then
+        echo "Installing the $2 is ... success"
+    else
+        echo "Installing the $2 is ... failure"
+        exit 1
+    fi
+}
+
+dnf list installed nginx
+if [ $? -ne 0]
+then
+    echo "nginx is not installed... installing now"
+
+    dnf install nginx -y
+    #validate installed or not
+    validate $? "nginx"
+else
+    echo "nginx is already installed.... Nothing to do"
+fi
+
